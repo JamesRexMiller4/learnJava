fd5d7e97289cda892cf1335abee3361d5c4474de
James Miller <james.rex.miller.4@gmail.com>
2020-05-03 14:46:09 -0600

    Add Char,Boolean,String practice

---
+++ Strings/src/com/jamesmiller/Main.java
@@ -0,0 +1,22 @@
+package com.jamesmiller;
+
+public class Main {
+
+    public static void main(String[] args) {
+//	    byte
+//      short
+//      int **
+//      long
+//      float
+//      double **
+//      char
+//      boolean **
+
+//      Most common primitive data-types **
+
+        String myString = "This is a string";
+        System.out.println(myString);
+        myString = myString + ", there are many like it, but this one is mine.";
+        System.out.println(myString);
+    }
+}
