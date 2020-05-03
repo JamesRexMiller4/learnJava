5cf3745d1f5e522ce4b3ae0bd543257f88aabdd8
James Miller <james.rex.miller.4@gmail.com>
2020-05-02 20:38:00 -0600

    Add ByteShortIntLong file, + another hello world for good measure

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,14 @@
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        int myFirstNumber = 5;
+        int mySecondNumber = 42;
+        int myThirdNumber = 19;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        System.out.println(myTotal);
+        int lastOne = myTotal - 1000;
+        System.out.println(lastOne);
+    }
+}
